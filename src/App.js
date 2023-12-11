7dafab40a114a16588f48e69ebe7fdeefacf4743
samir <samalam281@gmail.com>
2023-12-11 16:10:20 +0530

    frontendddd

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './components/Signup';
+import { Routes,Route } from 'react-router-dom';
+import Login from './components/Login';
+
+import Posts from './components/Posts';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+
+function App() {
+//   const [loggedIn, setLoggedIn] = useState(false);
+//   const handleLogout = () => {
+    
+//     setLoggedIn(false);
+// };
+const [loggedIn, setLoggedIn] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [postCount, setPostCount] = useState(0);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+    
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    setPosts([]);
+    setPostCount(0);
+  };
+
+  const handleDelete = (postId) => {
+    
+    const updatedPosts = posts.filter(post => post.id !== postId);
+    setPosts(updatedPosts);
+    setPostCount(updatedPosts.length);
+  };
+
+  const handleUpdate = (postId, updatedContent) => {
+    
+    const updatedPosts = posts.map(post => {
+      if (post.id === postId) {
+        return { ...post, content: updatedContent };
+      }
+      return post;
+    });
+    setPosts(updatedPosts);
+  };
+
+  return (
+    <div className="App">
+      <Navbar loggedIn={loggedIn} handleLogout={handleLogout} postCount={postCount} />
+    
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/register" element={<Signup/>} />
+      <Route path="/login" element={<Login  handleLogin={handleLogin}/>} />
+      <Route path="/posts" element={<Posts  loggedIn={loggedIn}
+            posts={posts}
+            handleDelete={handleDelete}
+            handleUpdate={handleUpdate}/>} />
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1> Socio masai</h1>
+
+    </div>
+  )
+}
+
+export default Home
